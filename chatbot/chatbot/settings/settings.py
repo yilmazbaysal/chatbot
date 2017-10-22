9de784e7f3f4b64ee7fc8cb3fa6adf2728c4d07b
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-10-22 10:34:10 +0300

    Question answering app added. Index page added.

--- chatbot/chatbot/settings/settings.py
+++ chatbot/chatbot/settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataset',
+    'question_answer',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -57,7 +58,9 @@ ROOT_URLCONF = 'chatbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, '..', 'question_answer', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

3982cc82158119598b88eb93307863d48ece5ca4
Yılmaz BAYSAL <baysalylmz@gmail.com>
2017-11-06 21:50:02 +0300

    File download feature was implemented.

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.conf.urls import url, include
+from django.conf import settings
+
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('question_answer.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

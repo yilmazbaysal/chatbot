6d99cde854e3d1f072e3d2c0899c4bc7c070a5ce
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-11-29 20:50:55 +0300

    A Django signal was implemented, to be able to reload data files after edited them via admin panel.

--- chatbot/dataset/apps.py
+++ chatbot/dataset/apps.py
@@ -6,6 +6,10 @@ class DatasetConfig(AppConfig):
 
     # This will run when the project is reloaded
     def ready(self):
+        # Register the signals
+        import dataset.signals
+
+        # Initial dataset load
         from question_answer.views import reload_dataset
 
         reload_dataset()

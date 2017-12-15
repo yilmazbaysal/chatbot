8d46f1ca40ea2ed966be43d7ffcb3db7f4fabbb7
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-12-15 15:08:02 +0300

    Bug fixes.

--- chatbot/dataset/forms.py
+++ chatbot/dataset/forms.py
@@ -4,45 +4,55 @@ from django.contrib.postgres.forms import SimpleArrayField, ValidationError
 from dataset.models import DataFile
 
 
-def lowercase_validator(keyword_list):
+def keyword_validator(keyword_list):
     """
     Checks the ArrayField whether it is all lowercase or not. Raise ValidationError if contains uppercase letter.
     :param keyword_list: List of keywords coming from an ArrayField.
     :return: No return value.
     """
-    for keyword in keyword_list:
-        if not keyword.islower():
-            raise ValidationError('The keywords must be all lowercase!', code='invalid')
+    for i in range(len(keyword_list)):
+        # Keyword length check
+        if len(keyword_list[i]) < 3:
+            raise ValidationError(
+                'Ensure the keyword {} has at least 3 characters (it has {}).'.format(i, len(keyword_list[i])),
+                code='invalid'
+            )
+
+        # Keyword character check
+        for letter in keyword_list[i]:
+            if not (letter.isdigit() or letter in ['-'] or letter.islower()):
+                raise ValidationError('The keywords must be all lowercase!', code='invalid')
 
 
 class GenerateDataFileForm(forms.ModelForm):
+    mnemonic = forms.CharField(min_length=3, required=True)
 
     keywords_1 = SimpleArrayField(
         forms.CharField(required=True),
         delimiter='|',
         required=True,
-        validators=[lowercase_validator],
+        validators=[keyword_validator],
         label='#1 Keyword List (Use "|" as delimiter)'
     )
     keywords_2 = SimpleArrayField(
         forms.CharField(required=True),
         delimiter='|',
         required=True,
-        validators=[lowercase_validator],
+        validators=[keyword_validator],
         label='#2 Keyword List (Use "|" as delimiter)'
     )
     keywords_3 = SimpleArrayField(
         forms.CharField(required=True),
         delimiter='|',
         required=False,
-        validators=[lowercase_validator],
+        validators=[keyword_validator],
         label='#3 Keyword List (Use "|" as delimiter)'
     )
     keywords_4 = SimpleArrayField(
         forms.CharField(required=True),
         delimiter='|',
         required=False,
-        validators=[lowercase_validator],
+        validators=[keyword_validator],
         label='#4 Keyword List (Use "|" as delimiter)'
     )
 

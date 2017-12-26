06e6457cbfc91724bb47c6698847e327fc82c30c
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-12-26 10:24:53 +0300

    Models and admin pages were added for stop-words and proper-nouns to be able to exclude them while spell-checking.

--- chatbot/question_answer/admin.py
+++ chatbot/question_answer/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
 
-# Register your models here.
+from question_answer.models import StopWord, ProperNoun
+
+
+def delete_selected(self, request, queryset):
+    for instance in queryset.all():
+        instance.delete()
+
+
+class StopWordAdmin(admin.ModelAdmin):
+    list_display = [
+        'keyword',
+    ]
+
+    list_filter = [
+        'keyword',
+    ]
+
+    exclude = [
+        'keyword_type',
+    ]
+
+    actions = [
+        delete_selected
+    ]
+
+    def get_queryset(self, request):
+        queryset = super(StopWordAdmin, self).get_queryset(request).filter(
+            keyword_type=StopWord.KEYWORD_TYPE_STOP_WORD
+        )
+
+        return queryset
+
+    def save_model(self, request, obj, form, change):
+        obj.keyword_type = StopWord.KEYWORD_TYPE_STOP_WORD
+        obj.save()
+
+
+class ProperNounAdmin(admin.ModelAdmin):
+    list_display = [
+        'keyword',
+    ]
+
+    list_filter = [
+        'keyword',
+    ]
+
+    exclude = [
+        'keyword_type',
+    ]
+
+    actions = [
+        delete_selected
+    ]
+
+    def get_queryset(self, request):
+        queryset = super(ProperNounAdmin, self).get_queryset(request).filter(
+            keyword_type=ProperNoun.KEYWORD_TYPE_PROPER_NOUN
+        )
+
+        return queryset
+
+    def save_model(self, request, obj, form, change):
+        obj.keyword_type = ProperNoun.KEYWORD_TYPE_PROPER_NOUN
+        obj.save()
+
+
+admin.site.register(StopWord, StopWordAdmin)
+admin.site.register(ProperNoun, ProperNounAdmin)
--- chatbot/question_answer/models.py
+++ chatbot/question_answer/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+    KEYWORD_TYPE_STOP_WORD = 'S'
+    KEYWORD_TYPE_PROPER_NOUN = 'P'
+
+    KEYWORD_TYPE_CHOICES = (
+        (KEYWORD_TYPE_STOP_WORD, 'Stop Word'),
+        (KEYWORD_TYPE_PROPER_NOUN, 'Proper Noun'),
+    )
+
+    keyword = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    keyword_type = models.CharField(max_length=1, choices=KEYWORD_TYPE_CHOICES, null=False, blank=False)
+
+
+class StopWord(Keyword):
+    class Meta:
+        proxy = True
+
+        verbose_name = 'Stop Word'
+        verbose_name_plural = 'Stop Words'
+
+
+class ProperNoun(Keyword):
+    class Meta:
+        proxy = True
+
+        verbose_name = 'Proper Noun'
+        verbose_name_plural = 'Proper Nouns'
